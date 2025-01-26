abf3885981be8d256f6d6a2776a8b5a7232a37b9
Andreas <git@fsck.rip>
2025-01-26 16:28:40 -0500

    handle external commands

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { createInterface } from "readline";
+import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
 import { type } from "./commands/type";
 import { echo } from "./commands/echo";
 
@@ -7,31 +8,72 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-export function loop() {
+export function loop(retCode?: number) {
   rl.question("$ ", (answer) => {
     const args = answer.split(" ");
 
+    if (retCode && retCode > 0) {
+      console.log("exit code %s", retCode);
+    }
+
+    // exit command
     if (args[0] == "exit") {
       rl.close();
       return;
     }
 
+    // echo command
     if (args[0] == "echo") {
-      args.shift(); 
-      echo(args.join(" ")).finally(loop)
+      args.shift();
+      echo(args.join(" ")).finally(loop);
       return;
     }
 
+    // type command
     if (args[0] == "type") {
       type(args[1]).finally(loop);
       return;
     }
 
-    if (args[0].length > 0) {
-      console.log(`${args[0]}: command not found`);
+    // handle system commands (not builtins)
+    const inputCmd = args[0];
+
+    // handle empty command
+    if (inputCmd.length < 1) {
+      loop();
+      return;
     }
 
-    loop();
+    args.shift(); // Remove the first arg (the cmd) to provide other args to process
+
+    let process: ChildProcessWithoutNullStreams;
+    try {
+      process = spawn(inputCmd, args);
+
+      process.stdout.on("data", (data) => {
+        console.log(`${data}`);
+        return;
+      });
+
+      process.stderr.on("data", (data) => {
+        console.error(`${data}`);
+        return;
+      });
+
+      process.on("close", (code) => {
+        loop(code as number);
+        return;
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        let code = (err as NodeJS.ErrnoException).code;
+        if (code == "ENOENT") {
+          console.log(`${inputCmd}: command not found`);
+        }
+      }
+      loop();
+      return;
+    }
   });
 }
 
